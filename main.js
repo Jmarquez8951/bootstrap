d8da0016b2206dc28e2b938f83a4e70f7c3db087
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-01-28 19:08:41 -0600

    added ducks array

--- main.js
+++ main.js
@@ -1,1 +1,100 @@
-console.log('js works');
+const ducks = [
+    {
+        color: 'blue',
+        isRubber: true,
+        gender: 'female',
+        isMigrator: false,
+        socialStatus: 'ready to mingle',
+        diet: 'vegan',
+        age: 47,
+        featherNum: 0,
+        name: 'Regina',
+        imgUrl: 'https://cdn11.bigcommerce.com/s-nf2x4/images/stencil/1280x1280/products/885/9633/Navy-Blue-Rubber-Duck-Adline-3__81336.1568313092.jpg?c=2&imbypass=on'
+    },
+    {
+        color: 'brown',
+        isRubber: false,
+        gender: 'male',
+        isMigrator: true,
+        socialStatus: 'not ready to mingle',
+        diet: 'meat',
+        age: 10,
+        featherNum: 2,
+        name: 'John',
+        imgUrl: 'https://www.allaboutbirds.org/guide/assets/photo/60291251-480px.jpg'
+    },
+    {
+        color: 'red',
+        isRubber: false,
+        gender: 'female',
+        isMigrator: false,
+        socialStatus: 'ready to mingle',
+        diet: 'vegan',
+        age: 5,
+        featherNum: 3300,
+        name: 'Samantha',
+        imgUrl: 'https://www.allaboutbirds.org/guide/assets/photo/60310501-480px.jpg'
+    },
+    {
+        color: 'blue',
+        isRubber: true,
+        gender: 'male',
+        isMigrator: false,
+        socialStatus: 'not ready to mingle',
+        diet: 'vegan',
+        age: 21,
+        featherNum: 0,
+        name: 'Bob',
+        imgUrl: 'https://www.amsterdamduckstore.com/wp-content/uploads/2019/12/Clouds-rubber-duck-front-Amsterdam-Duck-Store.jpg'
+    },
+    {
+        color: 'blue',
+        isRubber: false,
+        gender: 'female',
+        isMigrator: true,
+        socialStatus: 'not ready to mingle',
+        diet: 'meat',
+        age: 90,
+        featherNum: 5000,
+        name: 'Maria',
+        imgUrl: 'https://www.naturespic.com/i/34092BB00_w.jpg'
+    },
+    {
+        color: 'brown',
+        isRubber: true,
+        gender: 'male',
+        isMigrator: false,
+        socialStatus: 'ready to mingle',
+        diet: 'meat',
+        age: 30,
+        featherNum: 0,
+        name: 'Steven',
+        imgUrl: 'https://www.amsterdamduckstore.com/wp-content/uploads/2016/02/coffee-rubber-duck-leaning.jpg'
+    },
+    {
+        color: 'brown',
+        isRubber: true,
+        gender: 'male',
+        isMigrator: true,
+        socialStatus: 'not ready to mingle',
+        diet: 'pescatarian',
+        age: 50,
+        featherNum: 0,
+        name: 'Jimmy',
+        imgUrl: 'https://www.amsterdamduckstore.com/wp-content/uploads/2016/02/coffee-rubber-duck-leaning.jpg'
+    },
+    {
+        color: 'red',
+        isRubber: true,
+        gender: 'male',
+        isMigrator: true,
+        socialStatus: 'ready to mingle',
+        diet: 'vegetarian',
+        age: 14,
+        featherNum: 0,
+        name: 'Megan',
+        imgUrl: 'https://i.ebayimg.com/images/g/dGUAAOSw1LNZ4NAG/s-l300.jpg'
+    }
+];
+
+console.log(ducks);
